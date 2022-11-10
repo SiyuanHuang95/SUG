98dc4fd9a535768f0af78a57ad9d09b372a1f44c
SiyuanHuang95 <anthonyhuang4de@gmail.com>
2022-11-10 20:07:22 +0800

    abort fps, since it is too slow

--- data/dataloader.py
+++ data/dataloader.py
@@ -320,7 +320,13 @@ class UnifiedPointDG(data.Dataset):
                 shape=(self.num_points - pts.shape[0], 3), dtype=float)
             pts = np.concatenate((pts, pad_pc), axis=0)
         elif pts.shape[0] > self.num_points:
-            pts = fps(pts, self.num_points)
+            # fps 
+            # pts = fps(pts, self.num_points)
+
+            # random sample
+            point_idx = np.arange(0, pts.shape[0])
+            np.random.shuffle(point_idx)
+            pts = pts[point_idx[:self.num_points]]
         pts = np.expand_dims(pts.transpose(), axis=2)
         return torch.from_numpy(pts).type(torch.FloatTensor), label
 
--- tools/scripts/scripts_sproject/model_ablation_study.sh
+++ tools/scripts/scripts_sproject/model_ablation_study.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 
-#SBATCH --partition=shlab_adg_s2
+#SBATCH --partition=shlab_adg_s2_s2
 #SBATCH -N 1
 
 #SBATCH --output=/mnt/petrelfs/huangsiyuan/PointDG/logs_sproject/model_pn_%j.out
 #SBATCH --error=/mnt/petrelfs/huangsiyuan/PointDG/logs_sproject/model_pn_%j.err
 #SBATCH --gres=gpu:1
 #SBATCH --job-name=model_pn
+#SBATCH --job-name=model_pn
 #SBATCH  --quotatype=auto
+#SBATCH --cpus-per-task=4
 #SBATCH --cpus-per-task=4
 
 echo "SLURM_JOBID: " $SLURM_JOBID
 echo "Start Training"
 
 # python ../../../train_dg.py --source modelnet  --cfg ../../cfgs/cfgs_sproject/DG_unified_loss_onedataset_modelnet.yaml --ckpt_save_interval 40 --set METHODS.CLS_WEIGHT 10.0
+python ../../../train_dg_single_gpu.py --source modelnet  --cfg ../../cfgs/cfgs_sproject/DG_unified_loss_onedataset_modelnet_pointnet_ra_sub.yaml --ckpt_save_interval 40 --batch_size 64 --set Model Pointnet 
 python ../../../train_dg_single_gpu.py --source modelnet  --cfg ../../cfgs/cfgs_sproject/DG_unified_loss_onedataset_modelnet_pointnet_ra_sub.yaml --ckpt_save_interval 40 --batch_size 64 --set Model Pointnet 
 # python ../../../train_dg.py --source modelnet  --cfg ../../cfgs/cfgs_sproject/DG_unified_loss_onedataset_modelnet_tune.yaml --ckpt_save_interval 40
 
